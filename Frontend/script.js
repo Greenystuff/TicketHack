e9a1d1d1fb6ac948ebbf3b02e9b68b5a8660cc66
David Meunier <david_meunier@hotmail.fr>
2023-06-27 15:09:45 +0200

    commit

--- Frontend/script.js
+++ Frontend/script.js
@@ -6,3 +6,23 @@ Date.prototype.toDateInputValue = (function () {
 
 const todayDate = new Date('dd/mm/yyyy');
 document.querySelector('#calandar-input').value = new Date().toDateInputValue();
+
+document.querySelector('#search-btn').addEventListener('click', () => {
+  let departureValue = document.querySelector('#departure-input').value;
+  let arrivalValue = document.querySelector('#arrival-input').value;
+  let dateValue = document.querySelector('#calandar-input').value;
+  fetch('http://localhost:3000/trip-search', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ departure: departureValue, arrival: arrivalValue, date: dateValue }),
+  })
+    .then(data => data.json())
+    .then(trips => {
+      if (!trips.result) {
+        document.querySelector('#train-img').src = 'images/notfound.png'
+        document.querySelector('#desc-trip-txt').textContent = 'Aucun trajet trouvé...'
+      } else {
+
+      }
+    })
+})
