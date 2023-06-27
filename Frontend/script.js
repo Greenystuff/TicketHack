cf57e16d717fc94c0c7eebaf23a1bebe36f9f136
nadia11232000 <135129710+nadia11232000@users.noreply.github.com>
2023-06-27 15:19:55 +0200

    commit

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,28 +1,37 @@
-Date.prototype.toDateInputValue = (function () {
+Date.prototype.toDateInputValue = function () {
   var local = new Date(this);
   local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
   return local.toJSON().slice(0, 10);
-});
+};
 
-const todayDate = new Date('dd/mm/yyyy');
-document.querySelector('#calandar-input').value = new Date().toDateInputValue();
+const todayDate = new Date("dd/mm/yyyy");
+document.querySelector("#calandar-input").value = new Date().toDateInputValue();
 
-document.querySelector('#search-btn').addEventListener('click', () => {
-  let departureValue = document.querySelector('#departure-input').value;
-  let arrivalValue = document.querySelector('#arrival-input').value;
-  let dateValue = document.querySelector('#calandar-input').value;
-  fetch('http://localhost:3000/trip-search', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ departure: departureValue, arrival: arrivalValue, date: dateValue }),
+document.querySelector("#search-btn").addEventListener("click", () => {
+  let departureValue = document.querySelector("#departure-input").value;
+  let arrivalValue = document.querySelector("#arrival-input").value;
+  let dateValue = document.querySelector("#calandar-input").value;
+  fetch("http://localhost:3000/trip-search", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      departure: departureValue,
+      arrival: arrivalValue,
+      date: dateValue,
+    }),
   })
-    .then(data => data.json())
-    .then(trips => {
+    .then((data) => data.json())
+    .then((trips) => {
       if (!trips.result) {
-        document.querySelector('#train-img').src = 'images/notfound.png'
-        document.querySelector('#desc-trip-txt').textContent = 'Aucun trajet trouvé...'
+        document.querySelector("#train-img").src = "images/notfound.png";
+        document.querySelector("#desc-trip-txt").textContent =
+          "Aucun trajet trouvé...";
       } else {
-
+        document.querySelector("#result-container").innerHTML += `
+          <span>Paris > Lyon</span>
+          <span>18:48</span>
+          <span>53€</span>
+          <button class="book-btn">Book</button>`;
       }
-    })
-})
+    });
+});
